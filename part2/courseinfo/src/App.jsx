a7fa1c0d5b44b935152c9d395ec998a7d3ef343c
Yiwen <zhyiwen820@gmail.com>
2026-07-26 18:43:34 +0200

    finished whole courseinfo, the total hw costed 90 minutes

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,38 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>;
-
-const Content = (props) => (
-  <div>
-    {props.parts.map((info) => (
-      <Part key={info.id} name={info.name} exercises={info.exercises} />
-    ))}
-  </div>
-);
-
-const Part = (props) => (
-  <p>
-    {props.name} {props.exercises}
-  </p>
-);
-
-const Total = (props) => (
-  <p>Total of {props.exercises.reduce((acc, cur) => acc + cur, 0)} exercises</p>
-);
-
-const Course = (props) => (
-  <div>
-    <Header course={props.course.name} />
-    <Content parts={props.course.parts} />
-    <Total exercises={props.course.parts.map((part) => part.exercises)} />
-  </div>
-);
-
-const Courses = (props) => (
-  <div>
-    {props.courses.map((item) => (
-      <Course key={item.id} course={item} />
-    ))}
-  </div>
-);
+import Courses from "./Courses";
 
 const App = () => {
   const courses = [
