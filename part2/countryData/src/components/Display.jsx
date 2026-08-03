3c5b8fe0939d8133638b14633381bdbb338c487e
Yiwen <zhyiwen820@gmail.com>
2026-08-03 20:49:04 +0200

    finished ex 2.19, countryData step2

--- part2/countryData/src/components/Display.jsx
+++ part2/countryData/src/components/Display.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 const Display = ({ countries }) => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  useEffect(() => {
+    // Reset selected country if the countries prop changes
+    setSelectedCountry(null);
+  }, [countries]);
+    
+  const handleShowCountry = (countryName) => {
+    const country = countries.find((c) => c.name.common === countryName);
+    setSelectedCountry(country);
+    console.log("Selected country:", country);
+  };
+
   if (!Array.isArray(countries) || countries.length === 0) {
     return <p>No countries to display.</p>;
   } else if (countries.length > 10) {
@@ -9,23 +25,51 @@ const Display = ({ countries }) => {
       <div>
         <h2>{country.name.common}</h2>
         <p>Capital: {country.capital}</p>
-            <p>Population: {country.population}</p>
+        <p>Population: {country.population}</p>
         <h3>Languages:</h3>
         <ul>
-                {Object.values(country.languages).map((language, index) => (
-                    <li key={index}>{language}</li>
-                ))}
+          {Object.values(country.languages).map((language, index) => (
+            <li key={index}>{language}</li>
+          ))}
         </ul>
-        <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="200" />
+        <img
+          src={country.flags.png}
+          alt={`Flag of ${country.name.common}`}
+          width="200"
+        />
+      </div>
+    );
+  } else if (selectedCountry !== null) {
+    return (
+      <div>
+        <h2>{selectedCountry.name.common}</h2>
+        <p>Capital: {selectedCountry.capital}</p>
+        <p>Population: {selectedCountry.population}</p>
+        <h3>Languages:</h3>
+        <ul>
+          {Object.values(selectedCountry.languages).map((language, index) => (
+            <li key={index}>{language}</li>
+          ))}
+        </ul>
+        <img
+          src={selectedCountry.flags.png}
+          alt={`Flag of ${selectedCountry.name.common}`}
+          width="200"
+        />
       </div>
     );
   } else {
     return (
-      <ul>
-        {countries.map(country => (
-          <li key={country.cca3}>{country.name.common}</li>
+      <div>
+        {countries.map((country) => (
+          <div key={country.name.common}>
+            {country.name.common}{" "}
+            <button onClick={() => handleShowCountry(country.name.common)}>
+              Show
+            </button>
+          </div>
         ))}
-      </ul>
+      </div>
     );
   }
 };
